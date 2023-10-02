b754eaa5f599534ca8fccfe66d39fcf8cfd34266
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-10-02 20:11:31 -0300

    estrutura inicial

---
+++ src/main/java/com/api/vehiclepedia/VehiclepediaApplication.java
@@ -0,0 +1,13 @@
+package com.api.vehiclepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiclepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiclepediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/vehiclepedia/VehiclepediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.vehiclepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiclepediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
