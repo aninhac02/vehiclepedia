d4f75536eb65304c74b94e26ddb141ee99125faa
Ana Caroline Vilela <anacarolinevilela0@gmail.com>
2023-10-03 16:05:49 -0300

    criação do TruckController e MotorcycleController

---
+++ src/main/java/com/api/vehiclepedia/controller/MotorcycleController.java
@@ -0,0 +1,40 @@
+package com.api.vehiclepedia.controller;
+
+import com.api.vehiclepedia.model.entity.Motorcycle;
+import com.api.vehiclepedia.model.service.MotorcycleService;
+import com.api.vehiclepedia.utils.UrlsConsts;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/motos/marcas")
+public class MotorcycleController {
+
+    @Autowired
+    private MotorcycleService motorcycleService;
+    @GetMapping()
+    public String getBrands() {
+        return motorcycleService.getInfo(UrlsConsts.MOTORCYCLE_URL);
+    }
+
+    @GetMapping("/{brandCode}")
+    public String getModels(@PathVariable String brandCode) {
+        String url = UrlsConsts.MOTORCYCLE_URL + brandCode + UrlsConsts.MODELS_URL;
+        return motorcycleService.getInfo(url);
+    }
+
+    @GetMapping("/{brandCode}/{modelCode}")
+    public String getYears(@PathVariable String brandCode, @PathVariable String modelCode) {
+        String url = UrlsConsts.MOTORCYCLE_URL + brandCode + UrlsConsts.MODELS_URL + modelCode + UrlsConsts.YEARS_URL;
+        return motorcycleService.getInfo(url);
+    }
+
+    @GetMapping("/{brandCode}/{modelCode}/{yearCode}")
+    public Motorcycle getCar(@PathVariable String brandCode, @PathVariable String modelCode, @PathVariable String yearCode) {
+        String url = UrlsConsts.MOTORCYCLE_URL + brandCode + UrlsConsts.MODELS_URL + modelCode + UrlsConsts.YEARS_URL + yearCode;
+        return motorcycleService.getVehicle(url);
+    }
+}
